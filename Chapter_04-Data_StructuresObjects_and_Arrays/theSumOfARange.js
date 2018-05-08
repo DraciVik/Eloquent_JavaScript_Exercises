dd9d738505e9831994f9c4bf90cbc5cadc2be5a6
DraciVik <viktor.stojanov90@gmail.com>
2018-05-08 07:52:33 +0200

    Finished the Sum of a range challenge from chapter 04

--- Chapter_04-Data_StructuresObjects_and_Arrays/theSumOfARange.js
+++ Chapter_04-Data_StructuresObjects_and_Arrays/theSumOfARange.js
@@ -4,10 +4,37 @@ The introduction of this book alluded to the following as a nice way to compute 
 
 console.log(sum(range(1, 10)));
 Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
-
+*/
+function range(start, end) {
+  let arr = [];
+  for(start; start <= end; start++) {
+    arr.push(start);
+  }
+  return arr;
+}
+/*
 Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+*/
 
+function sum(array) {
+  let result = 0;
+  for(let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result;
+}
+/*
 As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+
+*/
+function range(start, end, step) {
+  let arr = [];
+  for(start; start <= end; start += step) {
+    arr.push(start);
+  }
+  return arr;
+}
+
 
 // Your code here.
 
@@ -18,3 +45,11 @@ console.log(range(5, 2, -1));
 console.log(sum(range(1, 10)));
 // → 55
 */
+
+function range(start, end) {
+  let arr = [];
+  for(start; start <= end; start++) {
+    arr.push(start);
+  }
+  return arr;
+}

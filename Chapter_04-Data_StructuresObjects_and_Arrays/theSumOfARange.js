74c6b16119aef55e34123cab3dc6368cf23799f9
DraciVik <viktor.stojanov90@gmail.com>
2018-05-10 22:38:51 +0200

    Finished the 'The sum of a range' challenge. Had to look up the bonus points assignment.

--- Chapter_04-Data_StructuresObjects_and_Arrays/theSumOfARange.js
+++ Chapter_04-Data_StructuresObjects_and_Arrays/theSumOfARange.js
@@ -27,19 +27,19 @@ function sum(array) {
 As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 
 */
-// function range(start, end, step) {
-//   let arr = [];
-//   if(step >= 0) {
-//   for(let i = start; i <= end; i += step) {
-//     arr.push(i);
-//   }
-// } else if(step < 0) {
-//   for(let i = start; i <= end; i -= step) {
-//     arr.push(i);
-//   }
-// }
-//   return arr;
-// }
+function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+  if(step > 0) {
+  for(let i = start; i <= end; i += step) {
+    array.push(i);
+  }
+} else {
+  for(let i = start; i >= end; i += step) {
+    array.push(i);
+  }
+}
+  return array;
+}
 
 /*
 // Your code here.

ca4ee46692b2f7452acab55a6e3b3ca566d6e37b
DraciVik <viktor.stojanov90@gmail.com>
2018-05-12 23:18:50 +0200

    Finished the list exercise. I had to look it up. Time to review the chapter and try again without looking at the code.

--- Chapter_04-Data_StructuresObjects_and_Arrays/aList.js
+++ Chapter_04-Data_StructuresObjects_and_Arrays/aList.js
@@ -49,3 +49,9 @@ function listToArray(list) {
   }
   return array;
 }
+
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1);
+}

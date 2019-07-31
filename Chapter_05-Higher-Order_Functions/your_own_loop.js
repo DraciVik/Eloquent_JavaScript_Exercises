09b30a4ae42a992294f62ccda35ababd56ac4538
DraciVik <viktor.stojanov90@gmail.com>
2019-07-31 23:49:49 +0200

    Wrote the solution in a wrong file. Fixed now

--- Chapter_05-Higher-Order_Functions/your_own_loop.js
+++ Chapter_05-Higher-Order_Functions/your_own_loop.js
@@ -10,3 +10,8 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 2
 // → 1
 */
+function loop(start, test, update, body) {
+        for (let value = start; test(value); value = update(value)) {
+                body(value);
+        }
+}

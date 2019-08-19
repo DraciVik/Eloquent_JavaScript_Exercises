1f3fcfdfa309a6a1eed1a1233205798634c5dc4d
DraciVik <viktor.stojanov90@gmail.com>
2019-08-19 23:18:15 +0200

    Solved 'A Vector Type' challenge from Chapter 6

--- Chapter-06-The-Secret-Life-Of-Objects/a-vector-type.js
+++ Chapter-06-The-Secret-Life-Of-Objects/a-vector-type.js
@@ -5,7 +5,24 @@
 // Add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y) from the origin (0, 0).
 
 // Your code here.
+class Vec {
+        constructor(x, y) {
+                this.x = x;
+                this.y = y;
+        }
 
+        plus(vector) {
+                return { x: this.x + vector.x, y: this.y + vector.y };
+        }
+
+        minus(vector) {
+                return { x: this.x - vector.x, y: this.y - vector.y };
+        }
+
+        get length() {
+                return Math.sqrt(this.x ** 2 + this.y ** 2);
+        }
+}
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
